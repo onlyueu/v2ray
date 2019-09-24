e3b4f1d117561f2ee0bb747c776413117f406c6e
onlyueu <55734657+onlyueu@users.noreply.github.com>
2019-09-24 15:30:30 +0800

    Create run_v2ray.sh

---
+++ run_v2ray.sh
@@ -0,0 +1,75 @@
+mv /etc/v2ray/config.json /etc/v2ray/config.json.bk
+port="$(grep 'port' /etc/v2ray/config.json.bk)"
+id="$(grep 'id' /etc/v2ray/config.json.bk)"
+oport="$(echo '"port": 11111,')"
+oid="$(echo '"id": "2a6af611-1111-4da3-b446-8866e398031a",')"
+
+cat >/etc/myconfig.conf <<EOL
+    {
+        "log": {
+                "access": "/var/log/v2ray/access.log",
+                "error": "/var/log/v2ray/error.log",
+                "loglevel": "warning"
+        },
+        "inbound": {
+                "port": 11111,
+                "protocol": "vmess",
+                "settings": {
+                        "clients": [{
+                                "id": "2a6af611-1111-4da3-b446-8866e398031a",
+                                "level": 1,
+                                "alterId": 64,
+                                "security": "auto"
+                        }]
+                },
+                "streamSettings":{
+                        "network":"ws",
+                        "security": "auto",
+                        "wsSettings":{
+                                "path": "/enterv2ray/"
+                                }
+                        }
+        },
+        "outbound": {
+                "protocol": "freedom",
+                "settings": {}
+        },
+        "outboundDetour": [
+            {
+                "protocol": "blackhole",
+                "settings": {},
+                "tag": "blocked"
+            }
+        ],
+        "routing": {
+            "strategy": "rules",
+            "settings": {
+                "rules": [
+                    {
+                        "type": "field",
+                        "ip": [
+                            "0.0.0.0/8",
+                            "10.0.0.0/8",
+                            "100.64.0.0/10",
+                            "127.0.0.0/8",
+                            "169.254.0.0/16",
+                            "172.16.0.0/12",
+                            "192.0.0.0/24",
+                            "192.0.2.0/24",
+                            "192.168.0.0/16",
+                            "198.18.0.0/15",
+                            "198.51.100.0/24",
+                            "203.0.113.0/24",
+                            "::1/128",
+                            "fc00::/7",
+                            "fe80::/10"
+                        ],
+                        "outboundTag": "blocked"
+                    }
+                ]
+            }
+        }
+    }
+EOF
+
+sed -e "s/$oport/$port/" -e "s/$oid/$id/" /etc/v2ray/config.json
